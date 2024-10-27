a02a024e3a55e1eef7b551485c315cc1757ad465
Omar <143974254+OmarDawoud4@users.noreply.github.com>
2024-10-27 21:56:09 +0300

    Delete The Bounded Buffer Problem directory

--- The Bounded Buffer Problem/utils.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "shared.h"
-
-extern int count; // Current count of items in the buffer
-
-void display_buffer_status() {
-    printf("Current buffer status: %d/%d items\n", count, BUFFER_SIZE);
-}
